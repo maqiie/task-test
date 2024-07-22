c43c7599ac1075a4d80ce5495d08dd20db9a4108
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-22 11:18:09 +0300

    added eslint lock file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-    // Your ESLint configuration settings
-    rules: {
-      'no-unused-vars': 'warn',
-      'react-hooks/exhaustive-deps': 'warn',
-      // Add other rules you want to change to warnings here
-    },
-  };
-  
+  // Your ESLint configuration settings
+  plugins: [
+    'react-hooks',
+    // other plugins
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    // Add other rules you want to change to warnings here
+  },
+  // other settings
+};
