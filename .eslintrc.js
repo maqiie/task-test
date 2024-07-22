9fd7a13d916775f69d4d904a51be8c5633c1d9be
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-22 11:09:12 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Your ESLint configuration settings
+    rules: {
+      'no-unused-vars': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      // Add other rules you want to change to warnings here
+    },
+  };
+  
