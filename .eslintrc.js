58080f6470ed522c9e42340eef5842e41913b229
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-22 11:31:54 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     // other plugins
   ],
   rules: {
-    'no-unused-vars': 'warn',
+          'no-unused-vars': 'warn',
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
     // Add other rules you want to change to warnings here
