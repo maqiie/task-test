6aec9232cf42e031c6d5212df7c1d07f480fef8c
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-25 12:09:42 +0300

    addedeslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,25 @@ module.exports = {
     // other plugins
   ],
   rules: {
-          'no-unused-vars': 'warn',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'no-unused-vars': 'warn', // Change 'no-unused-vars' rule to show warnings instead of errors
+    'react-hooks/rules-of-hooks': 'error', // Enforce rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Warn about missing dependencies in useEffect
     // Add other rules you want to change to warnings here
   },
-  // other settings
+  // Add other configurations if needed
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    // other configurations
+  ],
 };
