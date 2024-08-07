021be1fbd5c0b6900e987e55c9b33e0517e3538c
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-07 13:10:21 +0300

    worked on the elint

--- src/actionCableConsumer.js
+++ src/actionCableConsumer.js
@@ -1,35 +1,5 @@
+/* eslint-disable */
 
-// // export default cable;
-// import { createConsumer } from "@rails/actioncable";
-
-// // Ensure the URL is correct and uses wss:// for secure connections
-// const consumer = createConsumer("wss://task-test-backend.onrender.com/cable");
-
-// export default consumer;
-
-// actionCableConsumer.js
-// actionCableConsumer.js
-// import { createConsumer } from "@rails/actioncable";
-
-// const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-// const backendHost = 'task-test-backend.onrender.com';
-// const cableUrl = `${protocol}://${backendHost}/cable`;
-
-// const consumer = createConsumer(cableUrl);
-
-// consumer.connection.addEventListener('error', (error) => {
-//   console.error("WebSocket error:", error);
-// });
-
-// consumer.connection.addEventListener('close', (event) => {
-//   console.warn("WebSocket closed:", event);
-//   // Attempt to reconnect
-//   setTimeout(() => {
-//     consumer.connection.open();
-//   }, 5000);
-// });
-
-// export default consumer;
 
 import { createConsumer } from "@rails/actioncable";
 
