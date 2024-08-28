7a6504b418a9be3ecc39f7d98606536a50777a12
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-28 11:35:54 +0300

    removed the error

--- src/index.js
+++ src/index.js
@@ -12,21 +12,21 @@ root.render(
 );
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => console.log('Service Worker registered:', registration))
-      .catch(error => console.error('Service Worker registration failed:', error));
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => console.log('Service Worker registered:', registration))
+//       .catch(error => console.error('Service Worker registration failed:', error));
+//   });
+// }
 
-if (Notification.permission === 'default') {
-  Notification.requestPermission().then(permission => {
-    if (permission === 'granted') {
-      console.log('Notification permission granted.');
-    }
-  });
-}
+// if (Notification.permission === 'default') {
+//   Notification.requestPermission().then(permission => {
+//     if (permission === 'granted') {
+//       console.log('Notification permission granted.');
+//     }
+//   });
+// }
 
 
 
